92cf8e11a12dacd9bfe5f05daffb736fd6ec2af3
ZiadH225 <ziadhesham225@gmail.com>
2025-04-24 17:09:11 +0200

    Added Entities

---
+++ Social_Media_Application/Social_Media_Application/Common/Entities/Post.cs
@@ -0,0 +1,18 @@
+﻿namespace Social_Media_Application.Common.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string MediaUrl { get; set; }
+        public int LikesCount { get; set; }
+        public int CommentsCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+        public ICollection<PostLike> Likes { get; set; }
+    }
+}
---
+++ Social_Media_Application/Social_Media_Application/Common/Entities/PostLike.cs
@@ -0,0 +1,11 @@
+﻿namespace Social_Media_Application.Common.Entities
+{
+    public class PostLike
+    {
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
