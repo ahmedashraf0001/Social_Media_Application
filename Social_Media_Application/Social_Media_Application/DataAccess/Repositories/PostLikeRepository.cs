da05673c0f516c2eeceaca004fc6521982d345fa
ZiadH225 <ziadhesham225@gmail.com>
2025-04-26 18:58:42 +0300

    Added Comments and likes feature

--- Social_Media_Application/Social_Media_Application/DataAccess/Repositories/PostLikeRepository.cs
+++ Social_Media_Application/Social_Media_Application/DataAccess/Repositories/PostLikeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Social_Media_Application.Common.DTOs;
 using Social_Media_Application.Common.Entities;
 using Social_Media_Application.DataAccess.Data;
 using Social_Media_Application.DataAccess.Interfaces;
@@ -29,6 +30,22 @@ namespace Social_Media_Application.DataAccess.Repositories
         {
             var model = await GetLikeAsync(postId, userId);
             await DeleteAsync(model);
+        }
+        public async Task<List<UserLikeDTO>> GetPostLikesWithUsersAsync(int postId, int pageNumber, int pageSize = 12)
+        {
+            var query = _set
+                .Where(pl => pl.PostId == postId)
+                .Include(pl => pl.User)
+                .Select(pl => new UserLikeDTO
+                {
+                    UserId = pl.UserId,
+                    FullName = pl.User.FirstName + " " + pl.User.LastName,
+                    PhotoUrl = pl.User.PhotoUrl
+                });
+            return await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
         }
     }
 }
--- Social_Media_Application/Social_Media_Application/obj/Debug/net9.0/Social_Media_Application.AssemblyInfo.cs
+++ Social_Media_Application/Social_Media_Application/obj/Debug/net9.0/Social_Media_Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Social_Media_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213d2e04f9325b6921941e26fe4da1a17c7594fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+773757943ede54204a5ee209fe350454aaf7b7fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Social_Media_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Social_Media_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
